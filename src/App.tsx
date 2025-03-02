f6ba2a25aa0d1038be0c2be20a51d6f7488deeb2
Wendell Palazzo <wendell.palazzo@gmail.com>
2025-03-02 00:36:42 -0300

    adiciona scrollToTop para trabalhar com o react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Index from "./pages/Index";
 import Doe from "./pages/Doe";
 import ProjectDetails from "./pages/ProjectDetails";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/doe" element={<Doe />} />
---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({
+      top:0,
+      behavior:"instant"
+    });
+  }, [pathname]);
+
+  return null;
+}
