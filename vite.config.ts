d5fcbc3ab7f4955960a6acebc4b55dfae4aa8e0f
Wendell Palazzo <wendell.palazzo@gmail.com>
2025-03-05 16:41:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "inclusiva-cultura-design",
+  build:{
+    outDir:"./"
+  },
   server: {
     host: "::",
     port: 8080,
