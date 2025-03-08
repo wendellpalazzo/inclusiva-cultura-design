3288f4693edbb0a0944ae378f5a6ecbcdeb0ca8a
Wendell Palazzo <wendell.palazzo@gmail.com>
2025-03-08 17:38:19 -0300

    fix overflow

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen w-screen">
+    <div className="min-h-screen">
       <Header />
       <Hero />
       <AboutUs />
@@ -25,7 +25,7 @@ const Index = () => {
       <Testimonials />
       <Blog />
       <Contact />
-      <Accessibility />
+      {/* <Accessibility /> */}
       <Footer />
     </div>
   );
