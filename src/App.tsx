ff918f1fb44fdd48c6a564b502570d88ae0f6e2d
Wendell Palazzo <wendell.palazzo@gmail.com>
2025-04-12 17:43:31 -0300

    animações AOS

--- src/App.tsx
+++ src/App.tsx
@@ -13,35 +13,43 @@ import ScrollToTop from "./components/ScrollToTop";
 import { ReactLenis } from "lenis/react";
 import BlogDetails from "./pages/BlogDetails";
 import { HelmetProvider } from "react-helmet-async";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useLayoutEffect } from "react";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <HelmetProvider>
-  <ReactLenis root>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner richColors={true} expand={true} />
-        <BrowserRouter 
-        // basename="/inclusiva-cultura-design/"
-        >
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/doe" element={<Doe />} />
-            <Route path="/voluntarie-se" element={<Volunteer />} />
-            <Route path="/parcerias" element={<Partnership />} />
-            <Route path="/projetos/:slug" element={<ProjectDetails />} />
-            <Route path="/blog/:slug" element={<BlogDetails />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ReactLenis>
-  </HelmetProvider>
-);
+const App = () => {
+  useLayoutEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <ReactLenis root>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner richColors={true} expand={true} />
+            <BrowserRouter>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/doe" element={<Doe />} />
+                <Route path="/voluntarie-se" element={<Volunteer />} />
+                <Route path="/parcerias" element={<Partnership />} />
+                <Route path="/projetos/:slug" element={<ProjectDetails />} />
+                <Route path="/blog/:slug" element={<BlogDetails />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </TooltipProvider>
+        </QueryClientProvider>
+      </ReactLenis>
+    </HelmetProvider>
+  );
+};
 
 export default App;
